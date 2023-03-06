ffc16d3d093a05e75c454f5d164d8ef251734b65
Robbie Averill <robbie.averill@properly.ca>
2023-03-06 15:07:34 -0800

    fix: do not require extensions in import paths for js and ts files

--- index.js
+++ index.js
@@ -56,5 +56,16 @@ module.exports = {
       },
     ],
     'import/prefer-default-export': 'off',
+
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
   },
 };
